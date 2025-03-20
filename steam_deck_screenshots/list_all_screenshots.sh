14d5b9b8f86b8b4350d6d02ec4d431144a42f434
hardtopnet <push@hardtopnet.net>
2025-03-20 21:25:28 +0100

    update list_all_screenshots.sh to include optional parameters

--- steam_deck_screenshots/list_all_screenshots.sh
+++ steam_deck_screenshots/list_all_screenshots.sh
@@ -1,2 +1,9 @@
-find ~/Pictures -type d -name "thumbnails" -prune -o -type f -iname "*.??g" -print -follow
+#!/bin/bash
 
+# Default directory
+DIR=${1:-~/Pictures}
+# Default file extension filter
+EXT=${2:-*.??g}
+
+# Show all screenshots, excluding thumbnails. Filter by the provided extension
+find "$DIR" -type d -name "thumbnails" -prune -o -type f -iname "$EXT" -print -follow
