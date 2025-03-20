a67683daef2c253c79ac3a9d2c44aec8cb52d921
hardtopnet <push@hardtopnet.net>
2025-03-21 00:04:31 +0100

    getSteamGamesFromAPI.sh : extract constant for json file, change script_dir to the script's directory instead of hardcoded value

--- steam_deck_screenshots/getSteamGamesFromAPI.sh
+++ steam_deck_screenshots/getSteamGamesFromAPI.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-SCRIPT_DIR=~/scripts
-curl 'https://api.steampowered.com/ISteamApps/GetAppList/v2/' -o "$SCRIPT_DIR/steam-api.json"
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+$STEAM_API=steam-api.json
+
+curl 'https://api.steampowered.com/ISteamApps/GetAppList/v2/' -o "$SCRIPT_DIR/$STEAM_API"
